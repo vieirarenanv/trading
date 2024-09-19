bf751c57fc22e86917d361ec06ec306a0e2ca11a
Renan Vieira <vieirarenanv@gmail.com>
2024-09-18 22:45:40 -0300

    Atualizando página inicial e script

--- js/calculate.js
+++ js/calculate.js
@@ -14,14 +14,30 @@ function calculateTargets() {
     const target3 = entryPrice + 2 * stopDistance;
     const target4 = entryPrice + 2.5 * stopDistance;
 
+    function getDecimalPlaces(number) {
+        const numberString = number.toString();
+        if (numberString.indexOf('.') === -1) return 0;
+        return numberString.split('.')[1].length;
+    }
+
+    const decimalPlacesEntry = getDecimalPlaces(entryPrice);
+    const decimalPlacesStopLoss = getDecimalPlaces(stopLoss);
+    const decimalPlaces = Math.max(decimalPlacesEntry, decimalPlacesStopLoss);
+
+    const formattedTarget1 = target1.toFixed(decimalPlaces);
+    const formattedTarget2 = target2.toFixed(decimalPlaces);
+    const formattedTarget3 = target3.toFixed(decimalPlaces);
+    const formattedTarget4 = target4.toFixed(decimalPlaces);
+
     const resultsDiv = document.getElementById('results');
     resultsDiv.innerHTML = `
-        <p><strong>Alvo 1 </strong>➜ $${target1}</p>
-        <p><strong>Alvo 2 </strong>➜ $${target2}</p>
-        <p><strong>Alvo 3 </strong>➜ $${target3}</p>
-        <p><strong>Alvo 4 </strong>➜ $${target4}</p>
+        <p><strong>Alvo 1 </strong>➜ $${formattedTarget1}</p>
+        <p><strong>Alvo 2 </strong>➜ $${formattedTarget2}</p>
+        <p><strong>Alvo 3 </strong>➜ $${formattedTarget3}</p>
+        <p><strong>Alvo 4 </strong>➜ $${formattedTarget4}</p>
     `;
 }
+
 function resetForm() {
     document.getElementById('investmentForm').reset();
     document.getElementById('results').innerHTML = '';
